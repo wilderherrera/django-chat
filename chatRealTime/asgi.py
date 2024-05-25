00cf93e4794f3ea402621010f08f1015dbaf89f9
Wilder Herrera <wilder.herrera@unillanos.edu.co>
2024-05-24 19:21:05 -0500

    add redis service to github action

--- chatRealTime/asgi.py
+++ chatRealTime/asgi.py
@@ -13,6 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.url
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatRealTime.settings')
 
